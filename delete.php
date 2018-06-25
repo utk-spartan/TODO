0099940dd3cc3c7e93a898f683123c20db24d13e
Abhinav Venkatesh <abhinavvenkatesh@apples-MBP.localdomain>
2018-06-25 17:02:39 +0530

    fixed bugs

--- delete.php
+++ delete.php
@@ -1,36 +1,35 @@
 <?php
 
-require_once connect_db.php;
+require_once 'connect_db.php';
 
 // Define variables and initialize with empty values
 
 $id = "";
 
-if ($_SERVER['REQUEST_METHOD'] == "DELETE")
+if ($_SERVER['REQUEST_METHOD'] === "DELETE")
 {
-if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+
+    if (isset($_GET['id'])) {
 // get the 'id' variable from the URL
-    $id = $_GET['id'];
+    $id = (int)$_GET['id'];
 
     try {
 
-        $stmt = $conn->prepare("DELETE FROM activities WHERE taskid = (?)");
+        $stmt = $con->prepare("DELETE FROM tasks WHERE taskid = (?)");
         $stmt->bind_param("i", $id);
 
         $stmt->execute();
 
-        $conn->close();
+        $con->close();
 
     } catch (PDOException $e) {
         echo "There is some problem in connection: " . $e->getMessage();
     }
 }
-
+    else echo "invalid id";
 }
 
 ?>
-
-
 
 
 
