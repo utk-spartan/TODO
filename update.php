7b9e5cbc6c4a8831653d8d880a547c46d6fb0e71
Nisheet Kumar <nisheetkumar@apples-MBP-2.localdomain>
2018-06-25 18:45:02 +0530

    update an existing post

--- update.php
+++ update.php
@@ -1,15 +1,17 @@
+
 <?php
-require_once 'connect_db.php';
-    $user_id = $POST["id"];
+//require_once 'connect_db.php';
+$con = mysqli_connect("localhost","root","","todo");
+    //$user_id = $POST["id"];
     $task_id = $POST["taskID"];
     $task = $POST["task"];
-    $sql = "UPDATE tasks SET task=? WHERE taskID = ? && ID = ?";
+    $sql = "UPDATE tasks SET task=? WHERE taskID = ?";
     if($stmt = mysqli_prepare($con, $sql)) {
         // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "sssi", $param_task, $param_taskID, $param_ID);
+        mysqli_stmt_bind_param($stmt, "sssi", $param_task, $param_taskID);
         $param_task = $task;
         $param_taskID = $task_id;
-        $param_ID = $user_id;
+        //$param_ID = $user_id;
     }
 if(mysqli_stmt_execute($stmt)){
     echo "Task updated";
@@ -20,4 +22,3 @@ if(mysqli_stmt_execute($stmt)){
 mysqli_stmt_close($stmt);
 mysqli_close($con);
 ?>
-
