5642a08de6871504cff5108f7cfde9ed9347f6d5
Abhinav Venkatesh <abhinavvenkatesh@apples-MBP.localdomain>
2018-06-25 14:53:59 +0530

    delete operation initial draft

--- delete.php
+++ delete.php
@@ -3,57 +3,33 @@
 use connect_db.php;
 
 // Define variables and initialize with empty values
-$id = $name = "";
-
-$name_err = $id_err = "";
 
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-   /* $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
-        $name_err = "Please enter a name.";
-    }
-     else{
-        $name = $input_name;
-    }*/
-
-    $input_id = trim($_POST["id"]);
-    if(empty($input_id)){
-        $id_err = "Please enter id.";
-    }
-    else{
-        $id = $input_id;
-    }
-
-}
-
-try
+if _$SERVER['REQUEST_METHOD']
 {
-   // connect to databse
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+// get the 'id' variable from the URL
+    $id = $_GET['id'];
 
+    try {
 
+        $sql = "DELETE FROM activities WHERE taskid = $id";
 
-    $con =
+        if ($conn->query($sql) === TRUE) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: ";
+        }
 
-    $sql = "DELETE FROM activities WHERE taskid = some_value";
+        $conn->close();
 
-if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-} else {
-    echo "Error deleting record: " . $conn->error;
+    } catch (PDOException $e) {
+        echo "There is some problem in connection: " . $e->getMessage();
+    }
 }
 
-$conn->close();
-
-
-    echo "New record created successfully";
 }
 
-catch (PDOException $e)
-{
-    echo "There is some problem in connection: " . $e->getMessage();
-}
 ?>
 
 
@@ -64,5 +40,5 @@ catch (PDOException $e)
 
 
 
-?>
+
 
