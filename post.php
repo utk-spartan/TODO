114781aed6f0289a09d02a464dc93639edc5025b
Your Name <you@example.com>
2018-06-25 17:16:35 +0530

    Final post method

--- post.php
+++ post.php
@@ -1,57 +1,66 @@
 <?php
 
-use connect_db.php;
-$tsk_ID,$tsk,$U_ID;
+require_once "connect_db.php";
 
 
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+//
+//if($_SERVER["REQUEST_METHOD"] == "POST"){
+//
+//    /* $input_name = trim($_POST["name"]);
+//     if(empty($input_name)){
+//         $name_err = "Please enter a name.";
+//     }
+//      else{
+//         $name = $input_name;
+//     }*/
+//
+//    $input_id = trim($_POST["id"]);
+//    if(empty($input_id)){
+//        $id_err = "Please enter id.";
+//    }
+//    else{
+//        $id = $input_id;
+//    }
+//
+//}
 
-    /* $input_name = trim($_POST["name"]);
-     if(empty($input_name)){
-         $name_err = "Please enter a name.";
-     }
-      else{
-         $name = $input_name;
-     }*/
+var_dump($_POST);
 
-    $input_id = trim($_POST["id"]);
-    if(empty($input_id)){
-        $id_err = "Please enter id.";
+$activity=$_POST["input"];
+$act_id=$_POST["id"];
+    if($_SERVER["REQUEST_METHOD"]=="POST") {
+        $stmt = $con->prepare("INSERT INTO tasks (taskID,task) VALUES (?, ?)");
+        $stmt->bind_param("is", $task_ID, $task_name);
+        $task_ID =$act_id;
+        $task_name =$activity;
+        $stmt->execute();
+       
+        $stmt->close();
     }
     else{
-        $id = $input_id;
+        throw new Exception("Request method must be post");
     }
 
-}
-
-try
-{
-    // connect to databse
 
 
 
-    $conn =
-
-    $sql = "INSERT INTO tasks(taskID,task,ID)"
-    VALUES($tsk_ID,$tsk,$U_ID)
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Record added successfully";
-    } else {
-        echo "Error adding record: " . $conn->error;
-    }
-
-    $conn->close();
-
-
-    echo "New record created successfully";
-}
-
-catch (PDOException $e)
-{
-    echo "There is some problem in connection: " . $e->getMessage();
-}
+//    if ($conn->query($sql) === TRUE) {
+//        echo "Record added successfully";
+//    } else {
+//        echo "Error adding record: " . $conn->error;
+//    }
+//
+//    $conn->close();
+//
+//
+//    echo "New record created successfully";
+//}
+//
+//catch (PDOException $e)
+//{
+//    echo "There is some problem in connection: " . $e->getMessage();
+//}
 ?>
 
 
@@ -62,5 +71,5 @@ catch (PDOException $e)
 
 
 
-?>
+
 
